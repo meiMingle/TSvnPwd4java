ce6db59f75267ee0db7a174f8b727c4f041499f3
meiMingle <766781886@qq,com>
2020-12-05 00:46:54 +0800

    format the code

--- src/main/java/com/tomxin/tool/AuthParseException.java
+++ src/main/java/com/tomxin/tool/AuthParseException.java
@@ -2,38 +2,31 @@ package com.tomxin.tool;
 
 import java.util.*;
 
-public class AuthParseException extends RuntimeException
-{
+public class AuthParseException extends RuntimeException {
 
-  private String path;
-  private int lineNum;
+    private String path;
+    private int lineNum;
 
-  public AuthParseException(String path, int lineNum)
-  {
-	this.path = path;
-	this.lineNum = lineNum;
-  }
+    public AuthParseException(String path, int lineNum) {
+        this.path = path;
+        this.lineNum = lineNum;
+    }
 
-  public final String getPath()
-  {
-	  return path;
-  }
-  public final int getLineNum()
-  {
-	  return lineNum;
-  }
+    public final String getPath() {
+        return path;
+    }
 
-  @Override
-  public String getMessage()
-  {
-	if (lineNum != -1)
-	{
-	  return String.format("Error parsing line %1$s of %2$s", lineNum, path);
-	}
-	else
-	{
-	  return String.format("Error parsing %2$s", path);
-	}
-  }
+    public final int getLineNum() {
+        return lineNum;
+    }
+
+    @Override
+    public String getMessage() {
+        if (lineNum != -1) {
+            return String.format("Error parsing line %1$s of %2$s", lineNum, path);
+        } else {
+            return String.format("Error parsing %1$s", path);
+        }
+    }
 
 }
